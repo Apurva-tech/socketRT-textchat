424b40646fefd49963f626c2b67d62c38f7fd9a8
Apurva Sharma <apurva.sharma866@gmail.com>
2021-06-19 20:43:45 +0530

    fix: PORT changed to 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http); 
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 http.listen(PORT, () => {
